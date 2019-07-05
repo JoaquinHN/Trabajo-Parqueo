365b30331d84205dc2ebde73c0d9b802156f6fb2
DannyJ <dannycantarero3@gmail.com>
2019-07-04 19:37:07 -0600

    Update Query.sql
    Cambios a las tabas

--- Base de Datos/Query.sql
+++ Base de Datos/Query.sql
@@ -17,16 +17,30 @@ CREATE TABLE Parqueo.Automovil
 (
 	idAuto INT NOT NULL IDENTITY CONSTRAINT PK_Automovil_id PRIMARY KEY CLUSTERED,
 	placa VARCHAR(10) NOT NULL,
-	fechaEntrada DATETIME2 NOT NULL,
-	fechaSalida DATETIME2 NOT NULL,
-	tipoVehiculo VARCHAR(12) NOT NULL,
-	cobro MONEY NOT NULL
+	
 )
 GO
 
-CREATE TABLE Parqueo.Estacionamiento
+CREATE TABLE Parqueo.Transaccion
 (
-	idEstacionamiento INT NOT NULL IDENTITY CONSTRAINT PK_Estacionamiento_id PRIMARY KEY CLUSTERED,
-
-
+	idTran INT NOT NULL IDENTITY CONSTRAINT PK_Tran_id PRIMARY KEY CLUSTERED,
+	fechaEntrada DATETIME2 NOT NULL,
+	fechaSalida DATETIME2 NOT NULL,
+	cobro MONEY NOT NULL
 )
+
+CREATE TABLE Parqueo.TipoAutomovil
+(
+	idTipo INT NOT NULL IDENTITY CONSTRAINT PK_Tipo_id PRIMARY KEY CLUSTERED,
+	tipoVehiculo VARCHAR(12) NOT NULL,
+)
+GO
+
+
+ALTER TABLE Parqueo.Automovil  WITH CHECK ADD  CONSTRAINT FK_Parqueo_Automovil$EsUn$Parqueo_TipoAutomovil_id FOREIGN KEY(idAuto)
+REFERENCES Parqueo.TipoAutomovil (idTipo)
+GO
+
+ALTER TABLE Parqueo.Automovil  WITH CHECK ADD  CONSTRAINT FK_Parqueo_Automovil$TieneUna$Parqueo_Transaccion_id FOREIGN KEY(idAuto)
+REFERENCES Parqueo.Transaccion (idTran)
+GO
