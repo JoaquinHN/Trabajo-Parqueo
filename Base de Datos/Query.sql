ccb65ab0d2f3ac9fcabe22b02ccd298feff5d711
Alee2847L <51094270+Alee2847L@users.noreply.github.com>
2019-07-11 10:29:45 -0600

    Add files via upload
    cambio de hora a tabla Parqueo.Automovil

--- Base de Datos/Query.sql
+++ Base de Datos/Query.sql
@@ -15,36 +15,32 @@ GO
 
 CREATE SCHEMA Parqueo
 GO
+CREATE TABLE Parqueo.TipoAutomovil
+(
+	idTipo INT NOT NULL IDENTITY CONSTRAINT PK_Tipo_id PRIMARY KEY CLUSTERED,
+	tipoVehiculo NVARCHAR(12) NOT NULL,
+)
+GO
 
 CREATE TABLE Parqueo.Automovil
 (
 	idAuto INT NOT NULL IDENTITY CONSTRAINT PK_Automovil_id PRIMARY KEY CLUSTERED,
 	placa VARCHAR(10) NOT NULL,
 	idtipovehiculo INT NOT NULL,
-
-	
+		horaEntrada TIME NOT NULL
+		DEFAULT GETDATE()
 )
 GO
 
 CREATE TABLE Parqueo.Transaccion
 (
 	idTran INT NOT NULL IDENTITY CONSTRAINT PK_Tran_id PRIMARY KEY CLUSTERED,
-	fechaEntrada TIME NOT NULL
-		DEFAULT GETDATE(),
-	fechaSalida TIME NULL,
-	cobro MONEY NOT NULL,
+	horaSalida TIME NULL
+			DEFAULT GETDATE(),
+	cobro MONEY NULL,
 	idautomovil INT NOT NULl,
 )
 GO
-
-
-CREATE TABLE Parqueo.TipoAutomovil
-(
-	idTipo INT NOT NULL IDENTITY CONSTRAINT PK_Tipo_id PRIMARY KEY CLUSTERED,
-	tipoVehiculo VARCHAR(12) NOT NULL,
-)
-GO
-
 
 ALTER TABLE Parqueo.Automovil  WITH CHECK ADD  CONSTRAINT FK_Parqueo_Automovil$EsUn$Parqueo_TipoAutomovil_id FOREIGN KEY(idtipovehiculo)
 REFERENCES Parqueo.TipoAutomovil (idTipo)
@@ -54,9 +50,39 @@ ALTER TABLE Parqueo.Transaccion  WITH CHECK ADD  CONSTRAINT FK_Transaccion$Tiene
 REFERENCES Parqueo.Automovil (idAuto)
 GO
 
-INSERT INTO Parqueo.Transaccion
-	VALUES
-	(GETDATE(),GETDATE(),112)
+CREATE PROCEDURE AGREGARAUTO @placa VARCHAR(10), @idtipovehiculo INT
+AS
+begin TRANSACTION
+	BEGIN TRY
+			INSERT INTO Parqueo.Automovil(placa, idtipovehiculo)
+			VALUES (@placa, @idtipovehiculo);
+		COMMIT
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+	END CATCH
 GO
-SELECT*FROM Parqueo.Transaccion
+
+/*
+INSERT INTO Parqueo.TipoAutomovil
+	VALUES('Rastra'),
+		  ('Moto'),
+		  ('Bicicleta')
+INSERT INTO Parqueo.Automovil
+	VALUES('f25FF', 1)
+
+INSERT INTO Parqueo.Transaccion(idautomovil)
+	VALUES(1)
+
+
+
+EXEC AGREGARAUTO '05FG05', 1 
 GO
+SELECT * FROM Parqueo.Automovil
+GO
+SELECT * FROM Parqueo.Transaccion
+GO
+SELECT * FROM Parqueo.TipoAutomovil
+GO
+
+*/
