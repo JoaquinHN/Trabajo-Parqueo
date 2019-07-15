858173115657d3b082a3444724fe6183bec9e197
DannyJ <dannycantarero3@gmail.com>
2019-07-14 21:07:11 -0600

    Funcional, solo faltan restricciones

--- Estacionamiento/MainWindow.xaml.cs
+++ Estacionamiento/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Threading;
 using System.Data;
 using System.Data.SqlClient;
 using Estacionamiento.SQL;
+using System.Timers;
 
 namespace Estacionamiento
 {
@@ -27,11 +28,21 @@ namespace Estacionamiento
     {
 
         Clase_Conectar conexion = new Clase_Conectar();
+        Clase_Datos dat = new Clase_Datos();
         private DataTable tabla;
+
+        private int htotal;
+        private int hingreso;
+        private int hsalida;
+        private int idauto;
+
         public MainWindow()
         {
 
+
             InitializeComponent();
+
+
             tabla = new DataTable();
             try
             {
@@ -57,6 +68,17 @@ namespace Estacionamiento
             }
             MostrarAutos();
             MostrarHora();
+            
+
+            DispatcherTimer dispatcher = new DispatcherTimer();
+            dispatcher.Interval = new TimeSpan(0, 0, 1);
+            dispatcher.Tick += (s, a) =>
+            {
+                txthentrada.Text = DateTime.Now.ToString("hh:mm:ss.ms");
+                txths.Text = DateTime.Now.ToString("hh:mm:ss.ms");
+                
+            };
+            dispatcher.Start();
 
         }
 
@@ -75,13 +97,14 @@ namespace Estacionamiento
                     {
 
 
-                        string query = string.Format("AGREGARAUTO");
+                        string query = string.Format("AGREGARAUTOS");
                         SqlCommand comando = new SqlCommand(query, conexion.Conexion);
                         comando.CommandType = CommandType.StoredProcedure;
                         SqlDataAdapter adaptador = new SqlDataAdapter(comando);
                         using (adaptador)
                         {
                             comando.Parameters.AddWithValue("@placa", txtplaca.Text);
+                            comando.Parameters.AddWithValue("@horaEntrada", txthentrada.Text);
                             comando.Parameters.AddWithValue("@idtipovehiculo", cmbtipo.SelectedValue);
                             comando.ExecuteNonQuery();
                             MessageBox.Show(" Gracias por preferirnos");
@@ -132,35 +155,150 @@ namespace Estacionamiento
         }
         private void MostrarHora()
         {
-            try
-            {
-                string query = "SELECT * FROM Parqueo.Automovil";
-                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, conexion.Conexion);
-                using (sqlDataAdapter)
-                {
-                    DataTable tablaHora = new DataTable();
-                    sqlDataAdapter.Fill(tablaHora);
-                    lbhora.DisplayMemberPath = "horaEntrada";
-                    lbhora.SelectedValuePath = "idAuto";
-                    lbhora.ItemsSource = tablaHora.DefaultView;
-                }
-            }
-            catch (Exception e)
-            {
-
-                MessageBox.Show(e.ToString());
-            }
         }
 
         private void BtnCobro_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                conexion.Abrirconexion();
+                if (conexion.Estado == 1)
+                {
+
+
+                    string query = string.Format("AGREGARTRAN");
+                    SqlCommand comando = new SqlCommand(query, conexion.Conexion);
+                    comando.CommandType = CommandType.StoredProcedure;
+                    SqlDataAdapter adaptador = new SqlDataAdapter(comando);
+                    using (adaptador)
+                    {
+                        comando.Parameters.AddWithValue("@cobro", txttotal.Text);
+                        comando.Parameters.AddWithValue("@horaSalida", txths.Text);
+                        comando.Parameters.AddWithValue("@IdAuto", txtidauto.Text);
+                        comando.ExecuteNonQuery();
+                        MessageBox.Show(" Gracias por preferirnos");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(" Datos No Insertado" + ex.Message);
+            }
+
 
         }
 
+        private void BtnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtpla.Text == "")
+            {
+                MessageBox.Show("Ingrese una Placa");
+                txtpla.Focus();
+            }
+            else if (txtpla.Text != "")
+            {
+                dat.Placa = txtpla.Text;
+                dat.buscarplaca();
+                if (dat.Sec == 1)
+                {
+                    txtpla.Text = dat.Placa;
+                    txttip.Text = dat.Tipo;
+                    txthingre.Text = dat.Hingreso;
+                    txtidauto.Text = dat.IdAuto;
+                }
+
+                TimeSpan diferenciahoras = new TimeSpan();
+                DateTime horaingreso = new DateTime();
+                horaingreso = DateTime.Parse(txthingre.Text);
+
+                DateTime horasalida = new DateTime();
+                horasalida = DateTime.Parse(txths.Text);
+
+                diferenciahoras = horasalida - horaingreso;
+                int horas = diferenciahoras.Hours;
+                double horastotales = diferenciahoras.TotalHours;
+
+                txthtotal.Text = Convert.ToString(diferenciahoras);
+
+                if(txttip.Text=="1" || txttip.Text=="2" || txttip.Text=="3")
+                {
+                    if(horastotales>=0 && horastotales<=1)
+                    {
+                        txttotal.Text = "20";
+                    }
+                    if(horastotales>1 && horastotales<=2)
+                    {
+                        double s = 20+10;
+                        txttotal.Text = Convert.ToString(s);
+                    }
+                    if (horastotales > 2 && horastotales <= 4)
+                    {
+                        double s = (20*3)+10;
+                        txttotal.Text = Convert.ToString(s); ;
+                    }
+                    if(horastotales>=4)
+                    {
+                        double s = horastotales * 15;
+                        txttotal.Text = Convert.ToString(s);
+                    }
+                }
+                if(txttip.Text == "4" || txttip.Text == "5" || txttip.Text == "6")
+                {
+                    if (horastotales >= 0 && horastotales <= 1)
+                    {
+                        double s = 20 *2;
+                        txttotal.Text = Convert.ToString(s);
+                    }
+                    if (horastotales > 1 && horastotales <= 2)
+                    {
+                        double s = (20 + 10)*2;
+                        txttotal.Text = Convert.ToString(s);
+                    }
+                    if (horastotales > 2 && horastotales <= 4)
+                    {
+                        double s = ((20 * 3) + 10)*2;
+                        txttotal.Text = Convert.ToString(s); ;
+                    }
+                    if (horastotales >= 4)
+                    {
+                        double s = (horastotales * 15)*2;
+                        txttotal.Text = Convert.ToString(s);
+                    }
+                }
+                if(txttip.Text=="7")
+                {
+                    if (horastotales >= 0 && horastotales <= 1)
+                    {
+                        double s = (20)/2 ;
+                        txttotal.Text = Convert.ToString(s);
+                    }
+                    if (horastotales > 1 && horastotales <= 2)
+                    {
+                        double s = (20 + 10)/2;
+                        txttotal.Text = Convert.ToString(s);
+                    }
+                    if (horastotales > 2 && horastotales <= 4)
+                    {
+                        double s = ((20 * 3) + 10)/2;
+                        txttotal.Text = Convert.ToString(s); ;
+                    }
+                    if (horastotales >= 4)
+                    {
+                        double s = (horastotales * 15) /2;
+                        txttotal.Text = Convert.ToString(s);
+                    }
+
+                }
+
+            }
+        }
+
+        private void Txthtotal_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
-
-
-
 }
 
 
---
+++ Estacionamiento/SQL/Clase_Datos.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows;
+
+namespace Estacionamiento.SQL
+{
+    class Clase_Datos
+    {
+        private string placa;
+        private string tipo;
+        private string hingreso;
+        private int sec;
+        public string idAuto;
+        private DataTable tabla;
+
+        Clase_Conectar con = new Clase_Conectar();
+
+        public string Placa { get => placa; set => placa = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Hingreso { get => hingreso; set => hingreso = value; }
+        public int Sec { get => sec; set => sec = value; }
+        public string IdAuto { get => idAuto; set => idAuto = value; }
+       
+
+        public Clase_Datos()
+        {
+            idAuto ="";
+            placa = "";
+            tipo = "";
+            hingreso = "";
+            sec =0;
+            tabla = new DataTable();
+        }
+        
+         public void buscarplaca()
+        {
+            try
+            {
+                con.Abrirconexion();
+                if (con.Estado == 1)
+                {
+                    tabla.Reset();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(string.Format("select * from Parqueo.Automovil where placa='{0}'", Placa), con.Conexion);
+                    adaptador.Fill(tabla);
+                    if (tabla.Rows.Count > 0)
+                    {
+                        IdAuto = tabla.Rows[0][0].ToString();
+                        Sec = 1;
+                        Placa = tabla.Rows[0][1].ToString();
+                        Tipo = tabla.Rows[0][2].ToString();
+                        Hingreso = tabla.Rows[0][3].ToString();
+                    }
+                    else
+                    {
+                        Sec = 0;
+                        MessageBox.Show("Placa no encontrada");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
